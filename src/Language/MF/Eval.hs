205bfd9fa8e1bee5adb9cde3fb38a4396ba8a939
Juan Pablo García Garland <jpgarcia@fing.edu.uy>
2019-07-11 15:57:11 -0300

    add a simple evluator

---
+++ src/Language/MF/Eval.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE DataKinds #-}
+
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Language.Eval where
+
+import Language.MF.AST
+import Language.Grammars.AspectAG
+import Language.Grammars.AspectAG.TH
+
+import GHC.TypeLits (ErrorMessage(Text))
+import Data.Proxy
+
+{- Derived AST
+    data Exp
+      = UnOpApp {uop :: UnOp, e :: Exp} |
+        BinOpApp {l :: Exp, binop :: BinOp, r :: Exp} |
+        LitN {litN :: Int} |
+        LitR {litR :: Float} |
+        Arg {}
+      deriving (Show, Eq, Read)
+    data Func
+      = Func {body :: Exp}
+      deriving (Show, Eq, Read)
+-}
+
+$(attLabel "seval" ''Double)
+$(attLabel "iarg" ''Double)
+
+asp_iarg
+  =  traceAspect (Proxy @ ( 'Text "inhArg")) $
+     (inh iarg p_UnOpApp  ch_e    $ at lhs iarg)
+ .+: (inh iarg p_BinOpApp ch_l    $ at lhs iarg)
+ .+: (inh iarg p_BinOpApp ch_r    $ at lhs iarg)
+ .+: (inh iarg p_Func     ch_body $ at lhs iarg)
+ .+: emptyAspect
+
+
+asp_seval
+  = traceAspect (Proxy @ ( 'Text "synEval")) $
+     (syn seval p_Arg      $ at lhs iarg)
+ .+: (syn seval p_LitN     $ fromInteger <$> ter ch_litN)
+ .+: (syn seval p_LitR     $ ter ch_litR)
+ .+: (syn seval p_BinOpApp $
+     do l  <- at ch_l seval
+        r  <- at ch_r seval
+        op <- ter ch_binop
+        case op of
+          Plus -> return $ l + r
+          Div  -> return $ l / r
+     )
+ .+: (syn seval p_UnOpApp $
+     do e  <- at ch_e seval
+        op <- ter ch_uop
+        case op of
+          Inv -> return $ 1/e
+          Log -> return $ log e
+     )
+ .+: (syn seval p_Func $ at ch_body seval)
+ .+: emptyAspect
+
+asp_eval
+  = traceAspect (Proxy @ ('Text "join")) $ asp_seval .:+: asp_iarg
+
+-- eval :: Func -> Double -> Double
+eval f x = sem_Func asp_eval f (iarg =. x *. emptyAtt) #. seval
