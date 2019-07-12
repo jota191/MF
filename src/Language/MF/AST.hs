207269daffe2a88fb7a8fdfd998b56bdbb59636a
Juan Pablo García Garland <jpgarcia@fing.edu.uy>
2019-07-12 09:41:26 -0300

    update AST

--- src/Language/MF/AST.hs
+++ src/Language/MF/AST.hs
@@ -43,6 +43,23 @@ $(closeNTs [''Nt_Exp, ''Nt_Func])
 $(mkSemFuncs [''Nt_Exp, ''Nt_Func])
 
 
+-- | some test expressions
 e1 = Func $ BinOpApp (LitN 1) Div (BinOpApp (LitN 12) Plus (UnOpApp Log (Arg)))
 e2 = Func $ BinOpApp (LitN 24
                      ) Div (BinOpApp (LitN 12) Plus (UnOpApp Log (Arg)))
+
+e3 = Func $ BinOpApp (LitN 24
+                     ) Div (BinOpApp (LitN 12) Plus (UnOpApp Inv (Arg)))
+
+
+-- | identity aspect
+$(attLabel "sid" ''Exp)
+asp_id
+  =   (syn sid p_Arg      $ pure Arg)
+  .+: (syn sid p_LitR     $ LitR <$> ter ch_litR)
+  .+: (syn sid p_LitN     $ LitN <$> ter ch_litN)
+  .+: (syn sid p_BinOpApp
+      $ BinOpApp <$> at ch_l sid <*> ter ch_binop <*> at ch_r sid)
+  .+: (syn sid p_UnOpApp
+      $ UnOpApp <$> ter ch_uop <*> at ch_e sid)
+  .+: emptyAspect
