0ca8525da8dd1622283aa9c07e96be4a179d068e
Juan Pablo García Garland <jpgarcia@fing.edu.uy>
2019-07-21 21:19:09 -0300

    add some smt utils

--- src/Language/MF/SimpleSMTUtils.hs
+++ src/Language/MF/SimpleSMTUtils.hs
@@ -1,10 +1,21 @@
+{-# LANGUAGE RecordWildCards #-}
 
 module Language.MF.SimpleSMTUtils where
 
 import Math.SimpleSMT
+
 
 top = bool True
 bot = bool False
 
 neq :: SExpr -> SExpr -> SExpr
 neq a b = Math.SimpleSMT.not $  a `eq` b 
+
+
+-- non standard
+
+log :: SExpr -> SExpr
+log x = List [Atom "log", x]
+
+exp :: SExpr -> SExpr
+exp x = List [Atom "exp", x]
